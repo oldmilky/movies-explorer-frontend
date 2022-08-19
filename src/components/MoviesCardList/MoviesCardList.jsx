90cd9dea24e23d9f1734d21d455070a42a261d5e
oldmilky <oldmilky@yandex.kz>
2022-08-19 20:14:55 +0600

    lala

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-nested-ternary */
 import React from "react";
 import "./MoviesCardList.css";
 import { useLocation } from "react-router-dom";
