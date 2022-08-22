d3826ae52f76cb1f9a61bbdd1ba22c07819db564
oldmilky <77898003+oldmilky@users.noreply.github.com>
2022-08-22 16:25:20 +0600

    Update MoviesCardList.jsx

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-nested-ternary */
 import React from "react";
 import "./MoviesCardList.css";
 import { useLocation } from "react-router-dom";
