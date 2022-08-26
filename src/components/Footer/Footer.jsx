939f293a5571eb47900bbdb4337645fea9606025
oldmilky <oldmilky@yandex.kz>
2022-08-26 20:46:10 +0600

    asd

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Footer.css'
 
-function Footer(props) {
+function Footer() {
   return (
     <footer className="footer">
       <div className="footer__container">
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,45 +2,62 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Form.css";
 
-function Form({formName, submitText, children, registrationError, submitHandle, validation }) {
-  const {values, handleChange, errors, isValid, isFocused, onFocus} = validation
+function Form({
+  formName,
+  submitText,
+  children,
+  registrationError,
+  submitHandle,
+  validation,
+}) {
+  const { values, handleChange, errors, isValid, isFocused, onFocus } =
+    validation;
 
   return (
     <form className="form" onSubmit={submitHandle} name={`${formName}-form`}>
       {children}
-      <label htmlFor="email" className="form__label">E-mail</label>
-      <input 
-        required 
-        id="email" 
+      <label htmlFor="email" className="form__label">
+        E-mail
+      </label>
+      <input
+        required
+        id="email"
         name="email"
-        className={`form__input ${errors.email && 'form__input-error'}`}
-        minLength="2" 
-        type="email" 
-        value={values.email || ''}
+        className={`form__input ${errors.email && "form__input-error"}`}
+        minLength="2"
+        type="email"
+        value={values.email || ""}
         onFocus={onFocus}
         onChange={handleChange}
       />
-      <span className='form__input-error'>{isFocused && errors.email}</span>
-      <label htmlFor="password" className="form__label">Пароль</label>
-      <input 
-        required 
-        id="password" 
-        name="password" 
-        className={`form__input ${errors.password && 'form__input-error'}`}
-        minLength="2" 
+      <span className="form__input-error">{isFocused && errors.email}</span>
+      <label htmlFor="password" className="form__label">
+        Пароль
+      </label>
+      <input
+        required
+        id="password"
+        name="password"
+        className={`form__input ${errors.password && "form__input-error"}`}
+        minLength="2"
         type="text"
-        value={values.password || ''}
+        value={values.password || ""}
         onFocus={onFocus}
         onChange={handleChange}
       />
-      <span className='form__input-error'>{isFocused && errors.password}</span>
-      <button className="form__button" type="submit" disabled={!isValid}>{submitText.buttonText}</button>
-      {registrationError && <p className='form__input-error'>Произошла ошибка при регистрации</p>}
+      <span className="form__input-error">{isFocused && errors.password}</span>
+      <button className="form__button" type="submit" disabled={!isValid}>
+        {submitText.buttonText}
+      </button>
+      {registrationError && (
+        <p className="form__input-error">Произошла ошибка при регистрации</p>
+      )}
       <p className="form__promt">
         {`${submitText.promt} `}
-        <Link className="form__link" to={submitText.route}>{submitText.linkText}</Link>
+        <Link className="form__link" to={submitText.route}>
+          {submitText.linkText}
+        </Link>
       </p>
-      
     </form>
   );
 }
